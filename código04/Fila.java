4b321c77ee72a54c83feeeff8edb3c616e1ce71b
felpszg <felipegabriel@unipam.edu.br>
2026-09-03 23:19:21 +0000

    03/09

--- código04/Fila.java
+++ código04/Fila.java
@@ -1,49 +1,82 @@
 package código04;
+
 public class Fila<T>{
     private No<T> primeiroNo;
     private No<T> ultimoNo;
-    public String nomeFila;
+    private String nomeFila;
 
     public Fila(){
-        this.primeiroNo = null;
-        this.ultimoNo = null;
-        this.nomeFila = null;
+        this("");
     }
 
-    public Fila(String dado){
-        this.primeiroNo = null;
-        this.ultimoNo = null;
-        this.nomeFila = dado;
-    }
-
-
-
-
-
-    public No<T> getPrimeiroNo() {
-        return primeiroNo;
-    }
-    public void setPrimeiroNo(No<T> primeiroNo) {
-        this.primeiroNo = primeiroNo;
-    }
-    public No<T> getUltimoNo() {
-        return ultimoNo;
-    }
-    public void setUltimoNo(No<T> ultimoNo) {
-        this.ultimoNo = ultimoNo;
-    }
-    public String getNomeFila() {
-        return nomeFila;
-    }
-    public void setNomeFila(String nomeFila) {
+    public Fila(String nomeFila){
         this.nomeFila = nomeFila;
+        this.primeiroNo = null;
+        this.ultimoNo = null;
     }
 
+    public void enfileirar(T dado){
+        No<T> novoNo = new No<T>(dado);
+        if(primeiroNo == null){
+            primeiroNo = novoNo;
+            ultimoNo = novoNo;
+        } else {
+            ultimoNo.setNextNo(novoNo);
+            ultimoNo = novoNo;
+        }
+    }
 
+    public T desenfileirar(){
+
+        if (primeiroNo == null) {
+            System.out.println("Fila vazia!");
+            return null;
+        }
+
+        T dado = primeiroNo.getDado();
+        primeiroNo = primeiroNo.getNextNo();
+
+        if (primeiroNo == null) {
+            ultimoNo = null;
+        }
+        return dado;
+    }
+
+    public void imprimeFila(){
+        No<T> aux = primeiroNo;
+
+        while (aux != null) {
+            System.out.println("Dado: " + aux.getDado());
+            aux = aux.getNextNo();
+        }
+    }
 
 
 
 
 
     
+    public void setNomeFila(String nomeFila){
+        this.nomeFila = nomeFila;
+    }
+
+    public String getNomeFila(){
+        return this.nomeFila;
+    }
+
+    public void setPrimeiroNo(No<T> primeiroNo){
+        this.primeiroNo = primeiroNo;
+    }
+
+    public No<T> getPrimeiroNo(){
+        return this.primeiroNo;
+    }
+
+    public void setUltimoNo(No<T> ultimoNo){
+        this.ultimoNo = ultimoNo;
+    }
+
+    public No<T> getUltimoNo(){
+        return this.ultimoNo;
+    }
 }
